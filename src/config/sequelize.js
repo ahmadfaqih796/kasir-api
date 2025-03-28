a9b9fcffa0addbe3d2af52767ad771b05089379b
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-03-28 13:19:20 +0700

    sequelie

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -5,14 +5,15 @@ module.exports = async (app) => {
   app.set('sequelizeClient', sequelize);
 
   try {
-    if (DB_SYNC === 'true') {
-      await sequelize.sync();
-      console.log('Database synced');
-    } else {
-      await sequelize.authenticate();
-      console.log('Database connected');
-    }
-  } catch (error) {
-    console.error('Database connection failed:', error);
+    // if (DB_SYNC === 'true') {
+    //   // await sequelize.sync({ alter: true });
+    //   // console.log('Database synced');
+    // } else {
+    // }
+    await sequelize.authenticate();
+    console.log('Database connected');
+  } catch (_) {
+    // throw new Error('Database connection failed');
+    // console.error('Database connection failed:', error);
   }
 };
