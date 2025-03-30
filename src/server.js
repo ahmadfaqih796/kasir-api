01c0406afc1f327b876612f1499f7dd39e69bbb9
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-03-30 17:52:59 +0700

    build script

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const app = require('./app');
 
+require('dotenv').config();
+console.log("Running in mode:", process.env.NODE_ENV);
+
 const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => {
